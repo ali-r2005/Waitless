74e88efa6e86fa8bac4f78a121ca251c52cf6347
ali rami <ali.rami.6699@gmail.com>
2025-04-18 23:32:38 +0100

    fix branch model

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Branch extends Model
 {
-    protected $fillable = ['business_id','name','address'];
+    protected $fillable = ['business_id','name','address','parent_id'];
 
     public function business()
     {
